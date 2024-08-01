6f923470d0def95bca2ab0f612f199a508bb0351
codedreammer <nkanal38@gmail.com>
2024-08-01 19:58:14 +0530

    add the mdification

--- global variable .py
+++ global variable .py
@@ -1,10 +1,22 @@
-x = 10 
+x = 10
+print(x)
+
+def hello():
+    x = 5
+    print(f"the local x is {x}")
+    print("Hello akshay")
 
 def my_function():
     global x 
     x = 4
     y = 5 
     print(y)
+
+print(f"The local global x is {x}")
+hello()
+x= 5
+print(f"the global x is {x}")
+
 
 my_function()
 print(x)
